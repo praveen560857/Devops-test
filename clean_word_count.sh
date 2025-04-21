4fb4a0a7f7dbfae9a41c9523207c5b385ecfd407
praveen560857 <164129030+praveen560857@users.noreply.github.com>
2025-04-22 02:09:56 +0530

    Update clean_word_count.sh

--- clean_word_count.sh
+++ clean_word_count.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-[ -s input.txt ] && echo "Word Count:" || { echo "File is empty or missing."; exit 1; }
+file="input.txt"
 
-grep -vE '^#|^$' input.txt | \
-sed 's/[^a-zA-Z ]//g' | \
-tr 'A-Z' 'a-z' | \
-tr -s ' ' | \
-tr ' ' '\n' | \
-grep -v '^$' | \
-awk '{count[$1]++} END {for (w in count) printf "%-10s %d\n", w, count[w]}' | sort
+# If file exists AND is not empty
+if [ -f "$file" ] && [ -s "$file" ]; then
+  echo "Word Count:"
+  
+  grep -vE '^#|^$' "$file" | \
+  sed 's/[^a-zA-Z ]//g' | \
+  tr 'A-Z' 'a-z' | \
+  tr -s ' ' | \
+  tr ' ' '\n' | \
+  grep -v '^$' | \
+  awk '{count[$1]++} END {for (w in count) printf "%-10s %d\n", w, count[w]}' | sort
+
+else
+  echo "Error: File is missing or empty." >&2
+  exit 1
+fi
