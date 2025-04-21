7e4693e322c7b2af02d74a177388111247d21fad
praveen560857 <164129030+praveen560857@users.noreply.github.com>
2025-04-22 01:55:59 +0530

    Create clean_word_count.sh

---
+++ clean_word_count.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+[ -s input.txt ] && echo "Word frequency:" || { echo "File is empty or missing."; exit 1; }
+
+grep -vE '^#|^$' input.txt | \
+sed 's/[^a-zA-Z ]//g' | \
+tr 'A-Z' 'a-z' | \
+tr -s ' ' | \
+tr ' ' '\n' | \
+grep -v '^$' | \
+awk '{count[$1]++} END {for (w in count) printf "%-10s %d\n", w, count[w]}' | sort
