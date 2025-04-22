cb4bebb9c4356fd9cfcd4275e3dc4946021c665d
praveen560857 <164129030+praveen560857@users.noreply.github.com>
2025-04-22 12:50:35 +0530

    Update clean_word_count.sh

--- text_manipulation/clean_word_count.sh
+++ text_manipulation/clean_word_count.sh
@@ -4,15 +4,16 @@ file="input.txt"
 
 # If file exists AND is not empty
 if [ -f "$file" ] && [ -s "$file" ]; then
-  echo "Word Count:"
+  echo "Word frequency:"
   
   grep -vE '^#|^$' "$file" | \
-  sed 's/[^a-zA-Z ]//g' | \
+  sed "s/[^a-zA-Z']//g" | \
   tr 'A-Z' 'a-z' | \
   tr -s ' ' | \
   tr ' ' '\n' | \
   grep -v '^$' | \
-  awk '{count[$1]++} END {for (w in count) printf "%-10s %d\n", w, count[w]}' | sort
+  awk '{count[$1]++} END {for (w in count) printf "%-10s %d\n", w, count[w]}' | \
+  sort -k2,2nr -k1,1
 
 else
   echo "Error: File is missing or empty." >&2
